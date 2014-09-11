1708fe408f38843b6ec36a0fa1ebd4caa1f32e62
zhoubingzheng <zhoubingzheng@letv.com>
2014-09-11 19:21:41 +0800

    add the exception handler, when common exception, send email to mananger

--- cloudplatform-webportal-common/src/main/java/com/letv/common/spring/DefaultMappingExceptionResolver.java
+++ cloudplatform-webportal-common/src/main/java/com/letv/common/spring/DefaultMappingExceptionResolver.java
@@ -2,18 +2,30 @@ package com.letv.common.spring;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.letv.common.exception.CommonException;
+import com.letv.common.email.ITemplateMessageSender;
+import com.letv.common.email.bean.MailMessage;
 import com.letv.common.exception.ValidateException;
 import com.letv.common.result.ResultObject;
+import com.letv.common.session.Session;
+import com.letv.common.session.SessionManager;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,30 +33,40 @@ import org.slf4j.LoggerFactory;
 /**
  * 异常处理
  * 
- * @author liunaikun
  */
 public class DefaultMappingExceptionResolver extends SimpleMappingExceptionResolver {
 	private static final String ERROR_SYSTEM_ERROR = "系统出现异常，请稍后再试";
-
+	
 	private Logger logger = LoggerFactory.getLogger(DefaultMappingExceptionResolver.class);
+	
+	@Value("${error.email.to}")
+	private String ERROR_MAIL_ADDRESS;
+	
+	@Value("${error.email.enabled}")
+	private Boolean ERROR_MAIL_ENABLED;
+	
+	@Autowired
+	private ITemplateMessageSender defaultEmailSender;
+	
+	@Autowired
+	private SessionManager sessionManager;
 
     @Override
     protected ModelAndView doResolveException(HttpServletRequest req, HttpServletResponse res, Object handler,
             Exception e) {
-		/*if (e instanceof ValidateException) {
+		if (e instanceof ValidateException) {
 			responseJson(req, res, e.getMessage());
             return null;
-		} else if (e instanceof CommonException) {
-			responseJson(req, res, ERROR_SYSTEM_ERROR);
-
-			logger.error(ERROR_SYSTEM_ERROR, e);
-			return null;
-		} else {
-			responseJson(req, res, ERROR_SYSTEM_ERROR);
-
-			logger.error(ERROR_SYSTEM_ERROR, e);
-			return null;
-        }*/
+		}
+		
+		if(Boolean.valueOf(ERROR_MAIL_ENABLED))
+		{
+//			String stackTraceStr = this.getStackTrace(e);
+			String stackTraceStr = com.letv.common.util.ExceptionUtils.getRootCauseStackTrace(e);
+			String exceptionMessage = e.getMessage();
+			sendErrorMail(req,exceptionMessage,stackTraceStr);
+		}
+		
 		responseJson(req, res, e.getMessage());
 		logger.error(ERROR_SYSTEM_ERROR, e);
         return null;
@@ -73,5 +95,73 @@ public class DefaultMappingExceptionResolver extends SimpleMappingExceptionResol
 		out.flush();
 		out.close();
 
+	}
+	
+	private void sendErrorMail(HttpServletRequest request,String exceptionMessage,String stackTraceStr)
+	{
+		Map<String,Object> params = new HashMap<String,Object>();
+		params.put("exceptionContent", stackTraceStr);
+		params.put("requestUrl", request.getRequestURL());
+		
+		Session session = sessionManager.getSession();
+		params.put("exceptionId", session == null ? "error session" : session.getSeqNum());
+		
+		String requestValue = getRequestValue(request);
+		params.put("exceptionParams",  StringUtils.isBlank(requestValue) ? "无" : requestValue);
+		params.put("exceptionMessage",  exceptionMessage == null ? "无" : exceptionMessage);
+		
+		params.put("hostIp", request.getRemoteHost());
+		
+		
+		MailMessage mailMessage = new MailMessage("乐视云平台web-porta系统", ERROR_MAIL_ADDRESS,"异常错误发生","erroremail.ftl",params);
+		try {
+			defaultEmailSender.sendMessage(mailMessage);
+		} catch (Exception e) {
+			logger.error(getStackTrace(e));
+		}
+	}
+	
+	private String getStackTrace(Throwable t) 
+	{
+        StringWriter writer = new StringWriter();
+        PrintWriter pw = new PrintWriter(writer);
+        fillStackTrace(t, pw);
+        return writer.toString();
+    }
+	
+	private void fillStackTrace(Throwable t, PrintWriter pw)
+    {
+		Throwable ex = ExceptionUtils.getRootCause(t);
+		if(null == ex)
+			ex = t;
+        ex.printStackTrace(pw);
+    }
+	
+	private String getRequestValue(HttpServletRequest request)
+	{
+		StringBuilder sb = new StringBuilder();
+		Map<String,Object> requestParams = (Map<String,Object>)request.getParameterMap();
+		Set<String> keySets = requestParams.keySet();
+		for(String key:keySets)
+		{
+			String[] valueArrays = (String[])requestParams.get(key);
+			sb.append(key);
+			sb.append("=");
+			sb.append(getRequestItemValue(valueArrays));
+			sb.append("&");
+		}
+		String result = sb.toString();
+		return result;
+		
+	}
+	
+	private String getRequestItemValue(String[] valueArrays)
+	{
+		String valueStr = "";
+		for(String value:valueArrays)
+		{
+			valueStr += value;
+		}
+		return valueStr;
 	}
 }
