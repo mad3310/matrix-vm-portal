aeb2d160743116ae3cb4d2a0503e37e76ee28a7e
姚阔 <yaokuo@letv.com>
2014-12-10 09:53:14 +0800

    admin jsp与js分离

---
+++ cloudplatform-webportal-webadmin/src/main/webapp/static/scripts/pagejs/db_list.js
@@ -0,0 +1,353 @@
+var currentPage = 1; //第几页 
+var recordsPerPage = 10; //每页显示条数
+var currentSelectedLineDbName = 1;
+	
+ $(function(){
+	//初始化
+	page_init();
+
+	$(document).on('click', 'th input:checkbox' , function(){
+		var that = this;
+		$(this).closest('table').find('tr > td:first-child input:checkbox')
+		.each(function(){
+			this.checked = that.checked;
+			$(this).closest('tr').toggleClass('selected');
+		});
+	});
+	
+	//modal显示创建进度
+	var mclusterId;
+	$(document).on('click', "[name='buildStatusBoxLink']" , function(){
+		mclusterId = $(this).closest('tr').find('input').val();
+		if($(this).html().indexOf("正常")>=0){
+			$('#buildStatusHeader').html("创建成功");
+			status = "1";
+		}else if($(this).html().indexOf("创建中")>=0){
+			$('#buildStatusHeader').html("<i class=\"ace-icon fa fa-spinner fa-spin green bigger-125\"></i>创建中...");
+			status = "2";
+		}else if($(this).html().indexOf("创建失败")>=0){
+			$('#buildStatusHeader').html("创建失败");
+			status = "3";
+		}
+		queryBuildStatus(mclusterId,"new");
+	});
+	
+	$('#create-mcluster-status-modal').on('shown.bs.modal', function(){
+		if(status == "2") {
+			queryBuildStatusrefresh = setInterval(function() {  
+				queryBuildStatus(mclusterId,"update");
+			},5000);
+		}
+	}).on('hidden.bs.modal', function (e) {
+		queryBuildStatusrefresh = window.clearInterval(queryBuildStatusrefresh);
+		location.reload();
+	});
+});	
+function queryByPage(currentPage,recordsPerPage) {
+	$("#tby tr").remove();
+	var dbName = $("#nav-search-input").val()?$("#nav-search-input").val():'null';
+	$.ajax({ 
+		type : "get",
+		url : "/db/" + currentPage + "/" + recordsPerPage+"/" + dbName,
+		dataType : "json", /*这句可用可不用，没有影响*/
+		contentType : "application/json; charset=utf-8",
+		success : function(data) {
+			error(data);
+			var array = data.data.data;
+			var tby = $("#tby");
+			var totalPages = data.data.totalPages;
+			
+			for (var i = 0, len = array.length; i < len; i++) {
+				var td0 = $("<input class=\"hidden\" type=\"text\" value=\""+array[i].mclusterId+"\"\> ");
+				var td1 = $("<td class=\"center\">"
+								+"<label class=\"position-relative\">"
+								+"<input type=\"checkbox\" class=\"ace\"/>"
+								+"<span class=\"lbl\"></span>"
+								+"</label>"
+							+"</td>");
+				var td2;
+				if(array[i].status == 6){
+					td2 = $("<td>"
+							+ "<a href=\"/detail/db/"+array[i].id+"\">"+array[i].dbName+"</a>"
+							+ "</td>");
+				}else if(array[i].status == 0 ||array[i].status == 3){	
+					td2 = $("<td>"
+							+ "<a  class=\"danger\" href=\"/audit/db/"+array[i].id+"\">"+array[i].dbName+"</a>"
+							+ "</td>");
+				}else{
+					td2 = $("<td>"
+							+ "<a style=\"text-decoration:none;\">"+array[i].dbName+"</a>"
+							+ "</td>");
+				}
+				if(array[i].mcluster){
+					var td3 = $("<td>"
+ 							+ array[i].mcluster.mclusterName
+							+ "</td>");
+				} else {
+					var td3 = $("<td> </td>");
+				}
+				if(array[i].hcluster){
+					var td4 = $("<td>"
+ 							+ array[i].hcluster.hclusterNameAlias
+							+ "</td>");
+				} else {
+					var td4 = $("<td> </td>");
+				}
+				var td5 = $("<td>"
+						+ array[i].user.userName
+						+ "</td>");
+				var td6 = $("<td>"
+						+ date('Y-m-d H:i:s',array[i].createTime)
+						+ "</td>");
+				if(array[i].status == 4){
+					var td7 = $("<td>"
+							+"<a href=\"#\" name=\"dbRefuseStatus\" rel=\"popover\" data-container=\"body\" data-toggle=\"popover\" data-placement=\"top\" data-trigger='hover' data-content=\""+ array[i].auditInfo + "\" style=\"cursor:pointer; text-decoration:none;\">"
+							+ translateStatus(array[i].status)
+							+"</a>"
+							+ "</td>");
+				}else if(array[i].status == 2){
+					var td7 = $("<td>"
+							+"<a name=\"buildStatusBoxLink\" data-toggle=\"modal\" data-target=\"#create-mcluster-status-modal\" style=\"cursor:pointer; text-decoration:none;\">"
+							+"<i class=\"ace-icon fa fa-spinner fa-spin green bigger-125\" />"
+							+"创建中...</a>"
+							+ "</td>");
+				}else{
+					var td7 = $("<td> <a>"
+							+ translateStatus(array[i].status)
+							+ "</a></td>");
+				}
+				
+				/* var td6 = $("<td>"
+							+"<div class=\"hidden-sm hidden-xs action-buttons\">"
+								+"<a class=\"blue\" href=\"#\">"
+									+"<i class=\"ace-icon fa fa-play-circle-o bigger-130\"></i>"
+								+"</a>"
+								+"<a class=\"green\" href=\"#\">"
+									+"<i class=\"ace-icon fa  fa-power-off bigger-130\"></i>"
+								+"</a>"
+								+"<a class=\"red\" href=\"#\">"
+									+"<i class=\"ace-icon fa fa-trash-o bigger-130\"></i>"
+								+"</a>"
+							+"</div>"
+							+"<div class=\"hidden-md hidden-lg\">"
+								+"<div class=\"inline position-relative\">"
+									+"<button class=\"btn btn-minier btn-yellow dropdown-toggle\" data-toggle=\"dropdown\" data-position=\"auto\">"
+										+"<i class=\"ace-icon fa fa-caret-down icon-only bigger-120\"></i>"
+									+"</button>"
+									+"<ul class=\"dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close\">"
+										+"<li>"
+											+"<a href=\"#\" class=\"tooltip-info\" data-rel=\"tooltip\" title=\"View\">"
+												+"<span class=\"blue\">"
+													+"<i class=\"ace-icon fa fa-search-plus bigger-120\"></i>"
+												+"</span>"
+											+"</a>"
+										+"</li>"
+										+"<li>"
+											+"<a href=\"#\" class=\"tooltip-success\" data-rel=\"tooltip\" title=\"Edit\">"
+												+"<span class=\"green\">"
+													+"<i class=\"ace-icon fa fa-pencil-square-o bigger-120\"></i>"
+												+"</span>"
+											+"</a>"
+										+"</li>"
+										+"<li>"
+											+"<a href=\"#\" class=\"tooltip-error\" data-rel=\"tooltip\" title=\"Delete\">"
+												+"<span class=\"red\">"
+													+"<i class=\"ace-icon fa fa-trash-o bigger-120\"></i>"
+												+"</span>"
+											+"</a>"
+										+"</li>"
+									+"</ul>"
+								+"</div>"
+							+"</div>"
+						+"</td>"
+					);	 */
+					
+				if(array[i].status == 0 ||array[i].status == 5||array[i].status == 13){
+					var tr = $("<tr class=\"warning\"></tr>");
+					
+				}else if(array[i].status == 3 ||array[i].status == 4||array[i].status == 14){
+					var tr = $("<tr class=\"default-danger\"></tr>");
+					
+				}else{
+					var tr = $("<tr></tr>");
+				}
+				
+				tr.append(td0).append(td1).append(td2).append(td3).append(td4).append(td5).append(td6).append(td7);
+				tr.appendTo(tby);
+				
+				$('[name = "dbRefuseStatus"]').popover();
+			}//循环json中的数据 
+			
+			if (totalPages <= 1) {
+				$("#pageControlBar").hide();
+			} else {
+				$("#pageControlBar").show();
+				$("#totalPage_input").val(totalPages);
+				$("#currentPage").html(currentPage);
+				$("#totalRows").html(data.data.totalRecords);
+				$("#totalPage").html(totalPages);
+			}
+		},
+		error : function(XMLHttpRequest,textStatus, errorThrown) {
+			$.gritter.add({
+				title: '警告',
+				text: errorThrown,
+				sticky: false,
+				time: '5',
+				class_name: 'gritter-warning'
+			});
+	
+			return false;
+		}
+	});
+   }
+
+function pageControl() {
+	// 首页
+	$("#firstPage").bind("click", function() {
+		currentPage = 1;
+		queryByPage(currentPage,recordsPerPage);
+	});
+
+	// 上一页
+	$("#prevPage").click(function() {
+		if (currentPage == 1) {
+			$.gritter.add({
+				title: '警告',
+				text: '已到达首页',
+				sticky: false,
+				time: '5',
+				class_name: 'gritter-warning'
+			});
+	
+			return false;
+			
+		} else {
+			currentPage--;
+			queryByPage(currentPage,recordsPerPage);
+		}
+	});
+
+	// 下一页
+	$("#nextPage").click(function() {
+		if (currentPage == $("#totalPage_input").val()) {
+			$.gritter.add({
+				title: '警告',
+				text: '已到达末页',
+				sticky: false,
+				time: '5',
+				class_name: 'gritter-warning'
+			});
+	
+			return false;
+			
+		} else {
+			currentPage++;
+			queryByPage(currentPage,recordsPerPage);
+		}
+	});
+
+	// 末页
+	$("#lastPage").bind("click", function() {
+		currentPage = $("#totalPage_input").val();
+		queryByPage(currentPage,recordsPerPage);
+	});
+}
+//查询Container集群创建过程
+function queryBuildStatus(mclusterId,type) {	//type(update或new)
+	if(type == "new"){
+		$("#build_status_tby tr").remove();
+	}
+	$.ajax({
+		type : "get",
+		url : "/build/mcluster/"+mclusterId,
+		dataType : "json", /*这句可用可不用，没有影响*/
+		success : function(data) {
+			error(data);
+			var array = data.data;
+			var build_status_tby = $("#build_status_tby");
+			
+			for (var i = 0, len = array.length; i < len; i++) {
+				var td1 = $("<td>"
+						+ array[i].step
+						+"</td>");
+				var td2 = $("<td>"
+						+ array[i].stepMsg
+						+"</td>");
+				if(array[i].startTime != null )
+				{
+					var td3 = $("<td>"
+							+ date('Y-m-d H:i:s',array[i].startTime)
+							+ "</td>");
+				}else{
+					var td3 = $("<td>\-</td>");
+				}
+				if(array[i].endTime != null)
+				{
+					var td4 = $("<td>"
+							+ date('Y-m-d H:i:s',array[i].endTime)
+							+ "</td>");
+				}else{
+					var td4 = $("<td>\-</td>");
+				}
+				if(array[i].msg != null)
+				{
+					var td5 = $("<td>"
+							+ array[i].msg
+							+ "</td>");
+				}else{
+					var td5 = $("<td>\-</td>");
+				}
+				
+				if(array[i].status == 1){
+					var td6 = $("<td>"
+							+"<a class=\"green\"><i class=\"ace-icon fa fa-check bigger-120\">成功</a>"
+							+ "</td>");
+				}else if(array[i].status == 0){
+					var td6 = $("<td>"
+							+"<a class=\"red\"><i class=\"ace-icon fa fa-times red bigger-120\">失败</a>"
+							+ "</td>");
+				}else if(array[i].status == 2){
+					var td6 = $("<td>"
+							+"<a style=\"text-decoration:none;\" class=\"green\"><h5><i class=\"ace-icon fa fa-spinner fa-spin green bigger-120\"/>运行中</h5></a>"
+							+ "</td>");
+				}else{
+					var td6 = $("<td>"
+							+"<a class=\"orange\"><i class=\"ace-icon fa fa-coffee orange bigger-120\">等待</a>"
+							+ "</td>");
+				}
+					
+				if(array[i].status == 0){
+					var tr = $("<tr class=\"danger\"></tr>");
+				}else{
+					var tr = $("<tr></tr>");
+				}
+				
+				tr.append(td1).append(td2).append(td3).append(td4).append(td5).append(td6);
+				if(type == "new"){
+					tr.appendTo(build_status_tby);
+				}else{
+					build_status_tby.find("tr:eq("+i+")").html(tr.html());
+				}
+			}
+		},
+		error : function(XMLHttpRequest,textStatus, errorThrown) {
+			error(XMLHttpRequest);
+			return false;
+		}
+	});
+ }
+
+function searchAction(){
+	$('#nav-search-input').bind('keypress',function(event){
+        if(event.keyCode == "13")    
+        {
+        	queryByPage(currentPage, recordsPerPage);
+        }
+    });
+}
+function page_init(){
+	queryByPage(currentPage, recordsPerPage);
+	searchAction();
+	pageControl();
+}
---
+++ cloudplatform-webportal-webadmin/src/main/webapp/static/scripts/pagejs/hcluster_list.js
@@ -0,0 +1,230 @@
+var currentPage = 1; //第几页 
+var recordsPerPage = 15; //每页显示条数
+var queryBuildStatusrefresh;//刷新handler
+	
+$(function(){
+	//初始化 
+	page_init();
+	$('[name = "popoverHelp"]').popover();
+	$(document).on('click', 'th input:checkbox' , function(){
+		var that = this;
+		$(this).closest('table').find('tr > td:first-child input:checkbox')
+		.each(function(){
+			this.checked = that.checked;
+			$(this).closest('tr').toggleClass('selected');
+		});
+	});
+});
+
+function queryByPage(currentPage,recordsPerPage) {
+	$("#tby tr").remove();
+	var mclusterName = $("#nav-search-input").val()?$("#nav-search-input").val():'null';
+	$.ajax({
+		type : "get",
+		url : "/hcluster/" + currentPage + "/" + recordsPerPage + "/" + mclusterName,
+		dataType : "json", /*这句可用可不用，没有影响*/
+		success : function(data) {
+			if(error(data)) return;
+			var array = data.data.data;
+			var tby = $("#tby");
+			var totalPages = data.data.totalPages;
+			
+			for (var i = 0, len = array.length; i < len; i++) {
+				var td1 = $("<td class=\"center\">"
+								+"<label class=\"position-relative\">"
+								+"<input name=\"hcluster_id\" value= \""+array[i].id+"\" type=\"checkbox\" class=\"ace\"/>"
+								+"<span class=\"lbl\"></span>"
+								+"</label>"
+							+"</td>");
+				var td2 = $("<td>"
+						+ array[i].hclusterNameAlias
+						+ "</td>");
+				var td3 = $("<td>"
+						+  "<a href=\"/detail/hcluster/" + array[i].id+"\">"+array[i].hclusterName+"</a>"
+						+ "</td>");
+				var td4 = $("<td>"
+						+ date('Y-m-d H:i:s',array[i].createTime)
+						+ "</td>");
+				var td5 = $("<td><a>"
+						+ translateStatus(array[i].status)
+						+ "</a></td>");
+				var td6 = $("<td>"
+						+"<div class=\"hidden-sm hidden-xs  action-buttons\">"
+						+"<a class=\"red\" href=\"#\" onclick=\"deleteHcluster(this)\" data-toggle=\"modal\" data-target=\"#\">"
+							+"<i class=\"ace-icon fa fa-trash-o bigger-120\"></i>"
+						+"</a>"
+						+"</div>"
+						+ "</td>"
+				);
+					
+				if(array[i].status == 3){
+					var tr = $("<tr class=\"default-danger\"></tr>");
+				}else{
+					var tr = $("<tr></tr>");
+				}
+				
+				tr.append(td1).append(td2).append(td3).append(td4).append(td5).append(td6);
+				tr.appendTo(tby);
+			}//循环json中的数据 
+			
+			if (totalPages <= 1) {
+				$("#pageControlBar").hide();
+			} else {
+				$("#pageControlBar").show();
+				$("#totalPage_input").val(totalPages);
+				$("#currentPage").html(currentPage);
+				$("#totalRows").html(data.data.totalRecords);
+				$("#totalPage").html(totalPages);
+			}
+		}
+	});
+   }
+
+function pageControl() {
+	// 首页
+	$("#firstPage").bind("click", function() {
+		currentPage = 1;
+		queryByPage(currentPage,recordsPerPage);
+	});
+
+	// 上一页
+	$("#prevPage").click(function() {
+		if (currentPage == 1) {
+			$.gritter.add({
+				title: '警告',
+				text: '已到达首页',
+				sticky: false,
+				time: '5',
+				class_name: 'gritter-warning'
+			});
+	
+			return false;
+			
+		} else {
+			currentPage--;
+			queryByPage(currentPage,recordsPerPage);
+		}
+	});
+
+	// 下一页
+	$("#nextPage").click(function() {
+		if (currentPage == $("#totalPage_input").val()) {
+			$.gritter.add({
+				title: '警告',
+				text: '已到达末页',
+				sticky: false,
+				time: '5',
+				class_name: 'gritter-warning'
+			});
+	
+			return false;
+			
+		} else {
+			currentPage++;
+			queryByPage(currentPage,recordsPerPage);
+		}
+	});
+
+	// 末页
+	$("#lastPage").bind("click", function() {
+		currentPage = $("#totalPage_input").val();
+		queryByPage(currentPage,recordsPerPage);
+	});
+}
+
+function searchAction(){
+	$('#nav-search-input').bind('keypress',function(event){
+        if(event.keyCode == "13")    
+        {
+        	queryByPage(currentPage, recordsPerPage);
+        }
+    });
+}
+	
+//创建Container集群表单验证
+function formValidate() {
+	$("#create-hcluster-form").bootstrapValidator({
+	  message: '无效的输入',
+         feedbackIcons: {
+             valid: 'glyphicon glyphicon-ok',
+             invalid: 'glyphicon glyphicon-remove',
+             validating: 'glyphicon glyphicon-refresh'
+         },
+         fields: {
+       	  hclusterName: {
+                 validMessage: '请按提示输入',
+                 validators: {
+                     notEmpty: {
+                         message: '物理机集群名称不能为空!'
+                     },
+			          stringLength: {
+			              max: 40,
+			              message: '物理机集群名过长'
+			          },regexp: {
+		                  regexp: /^([a-zA-Z_0-9]*)$/,
+  		                  message: "请输入字母数字或'_'"
+                 	  },
+                 	 remote: {
+	                        message: '物理机集群名已存在!',
+	                        url: "/hcluster/validate"
+	                    }
+	             }
+         	}	
+         }
+     }).on('error.field.bv', function(e, data) {
+    	 $('#create-hcluster-botton').addClass("disabled");
+     }).on('success.field.bv', function(e, data) {
+    	 $('#create-hcluster-botton').removeClass("disabled");
+     });
+}
+
+function createHcluster(){
+	$.ajax({
+		type : "post",
+		url : "/hcluster",
+		data :$('#create-hcluster-form').serialize(),
+		success:function (data){
+			if(error(data)) return;
+			$('#create-hcluster-form').find(":input").not(":button,:submit,:reset,:hidden").val("").removeAttr("checked").removeAttr("selected");
+			$('#create-hcluster-form').data('bootstrapValidator').resetForm();
+			$('#create-hcluster-botton').addClass('disabled');
+			$('#create-hcluster-modal').modal('hide');
+			//延时一秒刷新列表
+			setTimeout("queryByPage(currentPage, recordsPerPage)",1000);
+		}
+	});
+}
+
+function deleteHcluster(obj){
+	var tr = $(obj).parents("tr");
+	var hclusterId =tr.find('[name="hcluster_id"]').val();
+	$.ajax({
+		url:'/hcluster/isExistHostOnHcluster/validate',
+		type:'post',
+		data:{ 'hclusterId' : hclusterId },
+		success:function(data){
+			if(data.valid){  //data.valid为true时可删除
+				function deleteCmd(){
+					$.ajax({
+						url:'/hcluster/'+hclusterId,
+						type:'delete',
+						success:function(data){
+							if(error(data)) return;
+							queryByPage(currentPage, recordsPerPage);
+						}
+					});
+				}
+				confirmframe("删除物理机集群","删除物理机集群后将不能恢复!","您确定要删除?",deleteCmd);
+			}else{
+				warn("该集群中含有物理机,删除完物理机后,才能执行此操作!",3000);
+			}
+		}
+	});
+}
+
+function page_init(){
+	queryByPage(currentPage, recordsPerPage);
+	searchAction();
+	formValidate();
+	pageControl();
+}
