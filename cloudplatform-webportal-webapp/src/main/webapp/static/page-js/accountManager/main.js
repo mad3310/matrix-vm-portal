fbcdbfbb8d1c96db6e3f9e4d183652aa9d1e9be6
姚阔 <yaokuo@letv.com>
2014-12-22 18:38:05 +0800

    添加dbuser列表

--- cloudplatform-webportal-webapp/src/main/webapp/static/page-js/accountManager/main.js
+++ cloudplatform-webportal-webapp/src/main/webapp/static/page-js/accountManager/main.js
@@ -5,13 +5,15 @@ define(function(require){
     var common = require('../common');
     var cn = new common();
 
+    var $ = require("jquery");
+
     /*初始化侧边栏菜单*/
     var index = [1,0];
     cn.Sidebar(index);//index为菜单中的排序(1-12)
     
     /*加载数据*/
     var dataHandler = require('./dataHandler');
-    var basicInfoHandler = new dataHandler();
+    var dbUserListHandler = new dataHandler();
 
-   // cn.GetData("/static/page-js/basicInfo/analogData/dblist.json",basicInfoHandler.BasicInfoHandler);
-});
+    cn.GetData("/dbUser/"+$("#dbId").val(),dbUserListHandler.DbUserListHandler);
+})
