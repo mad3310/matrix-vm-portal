4bed664e3ffbd2ddc4e42eadf2143f3821813670
姚阔 <yaokuo@letv.com>
2015-01-05 10:34:59 +0800

    添加监控数据

--- cloudplatform-webportal-webapp/src/main/webapp/static/page-js/monitor/dbLink/dataHandler.js
+++ cloudplatform-webportal-webapp/src/main/webapp/static/page-js/monitor/dbLink/dataHandler.js
@@ -6,11 +6,92 @@ define(function(require,exports,module){
 
     var DataHandler = function(){
     };
-
     module.exports = DataHandler;
 
+    var monitor = new DataHandler();
     DataHandler.prototype = {
-        DbListHandler : function(data){
+        InitCharts:function (data){
+            var div = $('#chart-container');
+            InitChart(div,data.titleText,data.yAxisText,data.tooltipSuffix);
+        },
+        SetChartData:function(data){
+            var dbId= $('#dbId').val();
+            var chart = $("#chart-container").highcharts();
+            var ydata = data.data;
+            for(var i=chart.series.length-1;i>=0;i--){
+                chart.series[i].remove(false);
+            }
+            for(var i=0;i<ydata.length;i++){
+                chart.addSeries(ydata[i],false);
+            }
+            chart.redraw();
         }
     }
+    function  InitChart(obj,title,ytitle,unit){
+        $(obj).highcharts({
+            chart: {
+                type: 'area',
+                zoomType: 'x',
+                spacingRight: 20
+            },
+            colors: ['#ff66cc','#66ff66','#66ffff','#FFBB33'],
+            title: {
+                text: title
+            },
+            legend :{
+                borderColor: '#000000',
+                backgroundColor: '#f9f9f9',
+                symbolRadius: '2px',
+                borderRadius: '5px',
+                itemHoverStyle: {
+                    Color: '#000000'
+                }
+            },
+            xAxis: {
+                type: 'datetime',
+                tickPixelInterval:150,
+                labels:{
+                    rotation:0,
+                    align:'right'
+                },
+                dateTimeLabelFormats:{
+                    millisecond: '%H:%M:%S.%L',
+                    second: '%H:%M:%S',
+                    minute: '%H:%M',
+                    hour: '%H:%M',
+                    day: '%e. %b',
+                    week: '%e. %b',
+                    month: '%b \'%y',
+                    year: '%Y'
+                }
+            },
+            plotOptions: {
+                area: {
+                    marker: {
+                        enabled: false,
+                        symbol: 'circle',
+                        radius: 2,
+                        states: {
+                            hover: {
+                                enabled: true
+                            }
+                        }
+                    }
+                }
+            },
+            credits:{
+                enabled: false
+            },
+            yAxis: {
+                title: {
+                    text: ytitle
+                }
+            },
+            tooltip: {
+                valueSuffix: "/s",
+                shared: true
+            }
+        });
+    }
 });
+
