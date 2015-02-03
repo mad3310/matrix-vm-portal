077392726d483ca895f37f6861d15ee30e3e278e
姚阔 <yaokuo@letv.com>
2015-02-03 17:23:32 +0800

    修复hclusterId=1问题

--- cloudplatform-webportal-webapp/src/main/webapp/static/page-js/common.js
+++ cloudplatform-webportal-webapp/src/main/webapp/static/page-js/common.js
@@ -422,13 +422,6 @@ define(function(require,exports,module){
 			$obj.css({'left':  objLeft + 'px','display': 'block'});
 		},
 		divselect: function () {
-			var inputselect = $('.divselect').find('input');
-			var divselects = $(".divselect");
-			/*for(var i = 0,len = divselects.length;i<len;i++){
-				if(divselects[i].find('li').length > 0){
-					divselects[i].
-				}
-			}*/
 			$('.divselect').closest('.pull-left').click(function(event){
 				event.stopPropagation();
 				var ul = $(this).find('ul');
@@ -447,7 +440,7 @@ define(function(require,exports,module){
 				var txt = $(this).find('a').text();
 				$(this).closest('.divselect').find('span').html(txt);
 				var value = $(this).find('a').attr("selectid");
-				inputselect.val(value);
+				$(this).closest('.divselect').find('input').val(value);
 			});
 			$(".divselect").each(function () {
 				if($(this).find('span').html() == ''&&$(this).find('li').length > 0){
