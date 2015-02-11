84aad4bfa59d2a1c041def869ee17e59eb2b60dc
姚阔 <yaokuo@letv.com>
2015-02-11 17:54:25 +0800

    判断监控返回数据为空时跳过。

--- cloudplatform-webportal-webadmin/src/main/webapp/static/scripts/pagejs/monitor_db_list.js
+++ cloudplatform-webportal-webadmin/src/main/webapp/static/scripts/pagejs/monitor_db_list.js
@@ -18,6 +18,7 @@ function queryMclusterMonitor() {
 			var tby = $("#tby");
 			
 			for (var i = 0, len = array.length; i < len; i++) {
+				if(array[i] == null) continue;
 				var mclusterName = '';
         		if(array[i].mcluster != undefined && array[i].mcluster != null) {
         			mclusterName = array[i].mcluster.mclusterName;
