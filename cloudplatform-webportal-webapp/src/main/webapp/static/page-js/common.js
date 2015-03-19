cef636cbdca594de478f92941162e3e8a3b23d44
jinglinlin <jinglinlin@letv.com>
2015-03-19 10:46:35 +0800

    json 格式化显示

--- cloudplatform-webportal-webapp/src/main/webapp/static/page-js/common.js
+++ cloudplatform-webportal-webapp/src/main/webapp/static/page-js/common.js
@@ -502,6 +502,75 @@ define(function(require,exports,module){
 			}else{
 				return "tcp";
 			}
+		},
+		formatJson:function(json){
+				var i           = 0,
+			        il          = 0,
+			        tab         = "    ",
+			        newJson     = "",
+			        indentLevel = 0,
+			        inString    = false,
+			        currentChar = null;
+				var cn = new Common;
+	
+			    for (i = 0, il = json.length; i < il; i += 1) { 
+			        currentChar = json.charAt(i);
+		
+			        switch (currentChar) {
+			        case '{': 
+			        case '[': 
+			            if (!inString) { 
+			                newJson += currentChar + "\n" + cn.repeat(tab, indentLevel + 1);
+			                indentLevel += 1; 
+			            } else { 
+			                newJson += currentChar; 
+			            }
+			            break; 
+			        case '}': 
+			        case ']': 
+			            if (!inString) { 
+			                indentLevel -= 1; 
+			                newJson += "\n" + cn.repeat(tab, indentLevel) + currentChar; 
+			            } else { 
+			                newJson += currentChar; 
+			            } 
+			            break; 
+			        case ',': 
+			            if (!inString) { 
+			                newJson += ",\n" + cn.repeat(tab, indentLevel); 
+			            } else { 
+			                newJson += currentChar; 
+			            } 
+			            break; 
+			        case ':': 
+			            if (!inString) { 
+			                newJson += ": "; 
+			            } else { 
+			                newJson += currentChar; 
+			            } 
+			            break; 
+			        case ' ':
+			        case "\n":
+			        case "\t":
+			            if (inString) {
+			                newJson += currentChar;
+			            }
+			            break;
+			        case '"': 
+			            if (i > 0 && json.charAt(i - 1) !== '\\') {
+			                inString = !inString; 
+			            }
+			            newJson += currentChar; 
+			            break;
+			        default: 
+			            newJson += currentChar; 
+			            break;                    
+			        } 
+			    } 
+		    return newJson; 
+		},
+		repeat:function(s, count){
+			 return new Array(count + 1).join(s);
 		}
 
 		 /*add new common function*/
