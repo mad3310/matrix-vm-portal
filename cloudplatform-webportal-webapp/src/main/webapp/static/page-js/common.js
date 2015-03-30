88d3b121dd382af5f8b3410695f76004add3f9fe
jinglinlin <jinglinlin@letv.com>
2015-03-30 17:59:03 +0800

    更新浏览器检测代码加载方式，放到jsp文件中

--- cloudplatform-webportal-webapp/src/main/webapp/static/page-js/common.js
+++ cloudplatform-webportal-webapp/src/main/webapp/static/page-js/common.js
@@ -10,8 +10,6 @@ define(function(require,exports,module){
 		this.maxConcurrency = 2000; //数据库最大并发量
 		
 		TopBtnInit();//初始化顶部菜单按钮
-		
-		browserVersion(); //浏览器检测初始化
     };
     module.exports = Common;
 
@@ -586,45 +584,6 @@ define(function(require,exports,module){
 			$(this).html("<i class=\"fa fa-home text-20\"></i>");
 		})
 	}
-    var browserInfo = function(){			
-		var bagent = navigator.userAgent.toLowerCase();
-		var regStr_ie = /msie [\d.]+;/gi ;
-		var regStr_ff = /firefox\/[\d.]+/gi
-		var regStr_chrome = /chrome\/[\d.]+/gi ;
-		var regStr_saf = /safari\/[\d.]+/gi ;
-		//IE
-		if(bagent.indexOf("msie") > 0){
-			return bagent.match(regStr_ie) ;
-		}
-
-		//firefox
-		if(bagent.indexOf("firefox") > 0){
-			return bagent.match(regStr_ff) ;
-		}
-
-		//Chrome
-		if(bagent.indexOf("chrome") > 0){
-			return bagent.match(regStr_chrome) ;
-		}
-
-		//Safari
-		if(bagent.indexOf("safari") > 0 && bagent.indexOf("chrome") < 0){
-			return bagent.match(regStr_saf) ;
-		}
-	}
-    var browserVersion = function(){
-    	  var _browser = browserInfo().toString().toLowerCase();
-    	  var verinfo = (_browser+"").replace(/[^0-9.]/ig,"");    	 
-    	  if(_browser.indexOf("msie") >=0 && (verinfo < 9.0)){
-    		  window.location.replace="/browserError";
-    	  }else if(_browser.indexOf("firefox") >=0 && verinfo < 15.0){
-    		  window.location.replace="/browserError";
-    	  }else if(_browser.indexOf("chrome") >=0 && verinfo < 21.0){
-    		  window.location.replace="/browserError";
-    	  }else if(_browser.indexOf("safari") >=0 && verinfo < 4.0){
-    		  window.location.replace="/browserError";
-    	  }
-    }
 });
 
 
