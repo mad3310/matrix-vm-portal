b9fb6450cae76a07002418c14fb8a61242f15d93
姚阔 <yaokuo@letv.com>
2015-03-31 18:23:34 +0800

    添加基本的 增删查

--- cloudplatform-webportal-data-model/src/main/java/com/letv/portal/dao/gce/IGceContainerDao.java
+++ cloudplatform-webportal-data-model/src/main/java/com/letv/portal/dao/gce/IGceContainerDao.java
@@ -1,8 +1,12 @@
 package com.letv.portal.dao.gce;
 
+import java.util.List;
+
 import com.letv.common.dao.IBaseDao;
+import com.letv.portal.model.ContainerModel;
 import com.letv.portal.model.gce.GceContainer;
 
 public interface IGceContainerDao extends IBaseDao<GceContainer> {
 
+	public  List<GceContainer> selectContainerByGceClusterId(Long clusterId);
 }
