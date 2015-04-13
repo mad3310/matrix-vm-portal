d422874ea5a361bd5134afb59ab49020aeec612a
李昀晖 <liyunhui@letv.com>
2015-04-13 18:26:13 +0800

    added cbase rest api

---
+++ cloudplatform-webportal-data-model/src/main/java/com/letv/portal/dao/ICbaseDao.java
@@ -0,0 +1,9 @@
+package com.letv.portal.dao;
+
+import com.letv.common.dao.IBaseDao;
+import com.letv.portal.model.CbaseClusterModel;
+import com.letv.portal.model.HclusterModel;
+
+public interface ICbaseDao extends IBaseDao<CbaseClusterModel>{
+
+}
