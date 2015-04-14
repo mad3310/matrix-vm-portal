1ea4f77e80877cfcb9c6c2a24e120be29572edaa
yaokuo <yaokuo@letv.com>
2015-04-14 16:29:08 +0800

    add gceImage

---
+++ cloudplatform-webportal-data-model/src/main/java/com/letv/portal/service/gce/IGceImageService.java
@@ -0,0 +1,8 @@
+package com.letv.portal.service.gce;
+
+import com.letv.portal.model.gce.GceImage;
+import com.letv.portal.service.IBaseService;
+
+public interface IGceImageService extends IBaseService<GceImage> {
+
+}
