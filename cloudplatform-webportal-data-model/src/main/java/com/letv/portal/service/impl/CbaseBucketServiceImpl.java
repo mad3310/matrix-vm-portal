eb26876a98087139f270cf89be1fbc985064140c
李昀晖 <liyunhui@letv.com>
2015-04-14 11:58:13 +0800

    add cbasebuck model

---
+++ cloudplatform-webportal-data-model/src/main/java/com/letv/portal/service/impl/CbaseBucketServiceImpl.java
@@ -0,0 +1,21 @@
+package com.letv.portal.service.impl;
+
+import com.letv.common.dao.IBaseDao;
+import com.letv.portal.model.CbaseBucketModel;
+import com.letv.portal.service.ICbaseBucketService;
+
+public class CbaseBucketServiceImpl extends BaseServiceImpl<CbaseBucketModel>
+implements ICbaseBucketService{
+
+	public CbaseBucketServiceImpl(Class<CbaseBucketModel> entityClass) {
+		super(entityClass);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public IBaseDao<CbaseBucketModel> getDao() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
