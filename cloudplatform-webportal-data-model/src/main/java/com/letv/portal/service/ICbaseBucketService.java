88d624b01178dd39e4d207eaf76fdfb7be822b78
李昀晖 <liyunhui@letv.com>
2015-04-17 17:21:41 +0800

    fix conflict

--- cloudplatform-webportal-data-model/src/main/java/com/letv/portal/service/ICbaseBucketService.java
+++
@@ -1,9 +0,0 @@
-package com.letv.portal.service;
-
-import com.letv.portal.model.cbase.CbaseBucketModel;
-
-
-public interface ICbaseBucketService extends IBaseService<CbaseBucketModel> {
-
-	public void dbList(Long cbaseId);
-}
