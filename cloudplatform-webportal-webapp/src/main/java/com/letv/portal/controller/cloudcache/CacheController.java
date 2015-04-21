c6941aa99da97303a8a6da241cc9c32f477c14cb
李昀晖 <liyunhui@letv.com>
2015-04-21 15:05:40 +0800

    add GET and POST /cbase, create cbase bucket success

--- cloudplatform-webportal-webapp/src/main/java/com/letv/portal/controller/cloudcache/CacheController.java
+++ cloudplatform-webportal-webapp/src/main/java/com/letv/portal/controller/cloudcache/CacheController.java
@@ -1,10 +1,32 @@
 package com.letv.portal.controller.cloudcache;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.NameValuePair;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.AuthCache;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.BasicAuthCache;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,7 +62,7 @@ public class CacheController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public @ResponseBody ResultObject list(Page page,
-			HttpServletRequest request, ResultObject obj) {
+			HttpServletRequest request, ResultObject result) {
 		Map<String, Object> params = HttpUtil.requestParam2Map(request);
 		params.put("createUser", sessionService.getSession().getUserId());
 		
@@ -48,14 +70,14 @@ public class CacheController {
 		if (!StringUtils.isEmpty(cacheName))
 			params.put("cacheName", StringUtil.transSqlCharacter(cacheName));
 		
-		obj.setData(this.cbaseBucketService.findPagebyParams(params, page));
-		return obj;
+		result.setData(this.cbaseBucketService.findPagebyParams(params, page));
+		return result;
 	}
 	
 	
 	@RequestMapping(method = RequestMethod.POST)
 	public @ResponseBody ResultObject save(CbaseBucketModel cbaseBucketModel,
-			ResultObject obj) {
+			ResultObject result) {
 //		if (cbaseBucketModel == null
 //				|| StringUtils.isEmpty(cbaseBucketModel.getBucketName())) {
 //			throw new ValidateException("参数不合法");
@@ -82,8 +104,70 @@ public class CacheController {
 		System.out.println("insert");
 		this.cbaseBucketService.insert(cbaseBucketModel);
 		
+		String name = cbaseBucketModel.getBucketName();
+		String ramQuotaMB = cbaseBucketModel.getRamQuotaMB();
+		String authType = "sasl";
+		Integer bucketType = cbaseBucketModel.getBucketType();
+		
+		
+		
 		//this.cbaseProxy.saveAndBuild(cbaseBucketModel);
-		return obj;
+		
+		CloseableHttpClient client = HttpClients.createDefault();
+
+		HttpHost targetHost = new HttpHost("10.154.156.57", 8091, "http");
+
+		CredentialsProvider credsProvider = new BasicCredentialsProvider();
+		credsProvider.setCredentials(AuthScope.ANY,
+				new UsernamePasswordCredentials("Administrator", "password"));
+
+		AuthCache authCache = new BasicAuthCache();
+		authCache.put(targetHost, new BasicScheme());
+
+		// Add AuthCache to the execution context
+		final HttpClientContext context = HttpClientContext.create();
+		context.setCredentialsProvider(credsProvider);
+		context.setAuthCache(authCache);
+
+		HttpPost postrequest = new HttpPost(
+				"http://10.154.156.57:8091/pools/default/buckets");
+
+		List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+		nvps.add(new BasicNameValuePair("name", name));
+		nvps.add(new BasicNameValuePair("ramQuotaMB", ramQuotaMB));
+		nvps.add(new BasicNameValuePair("authType", authType));
+		
+		// 非持久化 <=300MB
+		if(1==bucketType)
+			nvps.add(new BasicNameValuePair("bucketType", "memcached"));
+		// 持久化 MB >=100MB
+		else if(0==bucketType)
+			nvps.add(new BasicNameValuePair("replicaNumber", "2"));
+
+		try {
+			postrequest.setEntity(new UrlEncodedFormEntity(nvps));
+		} catch (UnsupportedEncodingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		try {
+			CloseableHttpResponse response = client.execute(targetHost,
+					postrequest, context);
+
+			HttpEntity entity = response.getEntity();
+			String responseBody = EntityUtils.toString(entity);
+
+			// System.out.println(responseBody);
+
+			result.setData(response.getStatusLine().toString());
+			result.addMsg(responseBody);
+
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		
+		return result;
 	}
 
 }
