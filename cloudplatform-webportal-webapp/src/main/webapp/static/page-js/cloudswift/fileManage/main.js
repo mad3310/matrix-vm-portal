a834ae32d2cdd032d6b14b3a96b3b852604561eb
gaomin <gaomin@CNBJWK43997GY.letv.local>
2015-06-30 09:58:50 +0800

    swift 文件上传 名称显示多‘/’ ，debug

--- cloudplatform-webportal-webapp/src/main/webapp/static/page-js/cloudswift/fileManage/main.js
+++ cloudplatform-webportal-webapp/src/main/webapp/static/page-js/cloudswift/fileManage/main.js
@@ -62,9 +62,11 @@ define(function(require){
 	$('#upload').change(function(event) {
 		if(cn.uploadfile(this)){//文件要求后缀和大小均符合
 			var file=cn.getFile(this);
-			var pathvalue=$('.dirPath').text();var path='root';
+			var pathvalue=$('.dirPath:visible').text();var path='root';
 			if(pathvalue){
-				path=$('#dirName').val();
+				if(pathvalue!='当前位置：根目录 /'){
+					path=$('#dirName').val();
+				}
 			}
 			if ($("#progress").length === 0) {
 			    $("body").append($("<div><dt/><dd/></div>").attr("id", "progress"));
