d213cb836cbd0728a9f88934829e87826a95fa8c
gaomin <gaomin@CNBJWK43997GY.letv.local>
2015-07-02 18:23:00 +0800

    数据库用户列表相关修改（修改权限、新建 未做）

--- cloudplatform-webportal-webadmin/src/main/webapp/static/scripts/pagejs/db_user_list.js
+++ cloudplatform-webportal-webadmin/src/main/webapp/static/scripts/pagejs/db_user_list.js
@@ -19,7 +19,7 @@ var currentSelectedLineDbName = 1;
 	});
 	
 	/*查询功能*/
-	$("#dbuserSearch").click(function(){
+	$("#dbuserSearch").unbind('click').click(function(){
 		var iw=document.body.clientWidth;
 		if(iw>767){//md&&lg
 		}else{
@@ -172,29 +172,48 @@ function queryByPage(){
 				var td3 = $("<td>"
 						+ "<a class=\"link\" href=\"/detail/db/" + array[i].dbId+"\">"+array[i].db.dbName+"</a>"
 						+ "</td>");
-				if(array[i].type == 1){
-					var td4 = $("<td>"
-							    + "管理员"
-							    + "</td>");
-				}else if(array[i].type == 2){
-					var td4 = $("<td>"
-						    + "只读用户"
-						    + "</td>");
-				}else{
-					var td4 = $("<td>"
-							    + "读写用户"
-							    + "</td>");
-				}
-				var td5 = $("<td>"
-						+ array[i].acceptIp
-						+ "</td>");
+				// if(array[i].type == 1){
+				// 	var td4 = $("<td>"
+				// 			    + "管理员"
+				// 			    + "</td>");
+				// }else if(array[i].type == 2){
+				// 	var td4 = $("<td>"
+				// 		    + "只读用户"
+				// 		    + "</td>");
+				// }else{
+				// 	var td4 = $("<td>"
+				// 			    + "读写用户"
+				// 			    + "</td>");
+				// }
+				// var td5 = $("<td>"
+				// 		+ array[i].acceptIp
+				// 		+ "</td>");
 				var td6 = $("<td class='hidden-480'>"
 						+ array[i].maxConcurrency
 						+ "</td>");
 				var td7 = $("<td><a>"
 						+ translateStatus(array[i].status)
-						+ "</a></td>"); 
-					
+						+ "</a></td>");
+				var td8; 
+				if(array[i].descn){
+					td8=$("<td class='hidden-480'><span>"
+						+array[i].descn
+						+"</span></td>");
+				}else{td8=$("<td class='hidden-480'><span>-"
+						+"</span></td>")}
+				
+				var td9=$("<td class='hidden-480'>" 
+						+ "<a class=\"dbuser-list-ip-privilege\" href='javascript:void(0)' data-db-id="+array[i].dbId+">ip访问权限</a><span class=\"text-explode\">"
+						+ "|</span><a class=\"dbuser-list-reset-password\"  href=\"javascript:void(0);\" data-db-id="+array[i].dbId+">重置密码</a><span class=\"text-explode\">"
+	                    + "|</span><a class=\"dbuser-list-modify-privilege disabled\" href=\"javascript:void(0);\">修改权限</a><span class=\"text-explode\">"
+	                    + "|</span><a class=\"dbuser-list-delete\"  href=\"javascript:void(0);\" data-db-id="+array[i].dbId+">删除</a></div>"
+						+"</td>"
+						+"<td class='hidden-lg hidden-md hidden-sm'>"
+						+ "<a class=\"dbuser-list-ip-privilege\" href=\"javascript:void(0);\"><span class='text-primary'><i class='fa fa-cogs'></i></span></a><span class=\"text-explode\">"
+						+ "|</span><a class=\"dbuser-list-reset-password\"  href=\"javascript:void(0);\"><i class='fa fa-key'></i></a><span class=\"text-explode\">"
+	                    + "|</span><a class=\"dbuser-list-modify-privilege\"  href=\"javascript:void(0);\"><i class='fa fa-edit'></i></a><span class=\"text-explode\">"
+	                    + "|</span><a class=\"dbuser-list-delete\"  href=\"javascript:void(0);\"><i class='fa fa-trash'></i></a> </div>"
+						+"</td>");	
 				if(array[i].status == 0 ||array[i].status == 5||array[i].status == 13){
 					var tr = $("<tr class=\"warning\"></tr>");
 				}else if(array[i].status == 3 ||array[i].status == 4||array[i].status == 14){
@@ -203,7 +222,8 @@ function queryByPage(){
 				}else{
 					var tr = $("<tr></tr>");
 				}
-				tr.append(td1).append(td2).append(td3).append(td4).append(td5).append(td6).append(td7);
+				// tr.append(td1).append(td2).append(td3).append(td4).append(td5).append(td6).append(td7);
+				tr.append(td1).append(td2).append(td6).append(td7).append(td3).append(td8).append(td9);
 				tr.appendTo(tby);
 			}
 			
@@ -216,6 +236,105 @@ function queryByPage(){
 				$("#totalRows").html(data.data.totalRecords);
 				$("#totalPage").html(totalPages);
 			}
+
+			// new add 2015-07-01-----------------------------------
+			/*获取行信息*/
+            function getLineData(obj){
+                var line = {
+                    "username":$(obj).closest("tr").find("td:eq(1)").html(),
+                    // "readWriterRate":$(obj).closest("tr").find("td:eq(2)").html(),
+                    "maxConcurrency":$(obj).closest("tr").find("td:eq(2)").html(),
+                    "descn":$(obj).closest("tr").find("td:eq(5) span").html()
+                }
+                return line;
+            }
+			var dbUserFuc={
+				GetDbUserPrivilege: function(data){
+			        var $tby = $("#ip-privilege-tby");
+			        $tby.find("tr").remove();
+			        $('#showDbuserIpPrivilege').modal({
+			            backdrop:false,
+			            show:true
+			        });
+			        var array = data.data;
+			        for(var i= 0,len=array.length;i<len;i++){
+			            if(array[i].used == 1){
+			                var td1 =  $("<td>"
+			                + array[i].addr
+			                + "</td>");
+			                var td2 =  $("<td>"
+			                + TranslateDbUserType(array[i].type)
+			                + "</td>");
+			                var tr =$("<tr></tr>");
+			                tr.append(td1).append(td2);
+			                tr.appendTo($tby);
+			            }
+			        }
+			    },
+		        DeleteDbUser:function(name,id){
+		            var username = name;
+		            var url= "/dbUser/"+id+"/"+username;
+		            $.ajax({
+		            	url:url,
+		            	type: 'delete',
+		            	dataType: 'json',
+		            	success:function(data){
+		            		if(error(data)){return;}
+		            	}
+		            });
+		        }
+			}
+			/*初始化查看用户权限按钮*/
+			$(".dbuser-list-ip-privilege").unbind('click').click(function () {
+			    var lineData = getLineData(this);
+			    var dbId=$(this).attr('data-db-id');
+			    $.ajax({
+			    	url: "/dbIp/"+dbId+"/"+lineData.username,
+			    	type: 'get',
+			    	success:function(data){
+			    		console.log(error(data)+data.result)
+			    		if(error(data)){return;}
+			    		dbUserFuc.GetDbUserPrivilege(data);
+			    	}
+			    });
+			    $("#showDbuserIpPrivilegeTitle").html(lineData.username);
+			});
+			/*初始化重置密码*/
+            $(".dbuser-list-reset-password").unbind('click').click(function () {
+                var lineData = getLineData(this);
+                $("#reset-password-box-title").html("重置账户("+lineData.username+")密码");
+                $("#reset-password-box").modal({
+                    backdrop:false,
+                    show:true
+                });
+                $("#reset-password-username").val(lineData.username);
+                var dbId=$(this).attr('data-db-id');
+                $("#reset-password-username").attr('data-db-id', dbId);
+            });
+            /*初始化删除按钮*/
+            $(".dbuser-list-delete").unbind('click').click(function () {    
+                var lineData = getLineData(this);
+                var title = "确认";
+                var text = "您确定要删除("+lineData.username+")账户";
+                var name = lineData.username;
+                var dbId=$(this).attr('data-db-id');
+                DialogBoxInit(title,text,dbUserFuc.DeleteDbUser(name,dbId));
+            });
+            // /*初始化修改用户权限按钮*/
+            // $(".dbuser-list-modify-privilege").unbind('click').click(function(){
+            //     $("#newAccountTab").addClass("hide");
+            //     $("#accountList").addClass("hide");
+            //     $("#modifyAccountTab").removeClass("hide");
+            //     var dbId=$(this).attr('data-db-id');
+            //     var lineData = getLineData(this);
+            //     $.get("/dbIp/"+dbId+"/"+lineData.username, function(data) {
+            //     	console.log('aaa')
+            //     });
+            //     $("#modifyFormDbUsername").html(lineData.username);
+            //     $("#modifydbUserMaxConcurrency").val(lineData.maxConcurrency);
+            //     $("#modifydbUserReadWriterRate").val(lineData.readWriterRate);
+            //     $("#modifyFormDbDesc").html(lineData.descn);
+            // })
 		},
 		error : function(XMLHttpRequest,textStatus, errorThrown) {
 			$.gritter.add({
@@ -230,6 +349,7 @@ function queryByPage(){
 		}
 	});
 }
+
 function pageControl() {
 	// 首页
 	$("#firstPage").bind("click", function() {
@@ -281,7 +401,114 @@ function pageControl() {
 		queryByPage();
 	});
 }
+//修改密码表单验证
+function formValidate() {
+	$('#reset-password-form').bootstrapValidator({
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            'reset-password': {
+                validators: {
+                    notEmpty: {
+                        message:'密码不能为空'
+                    },regexp: {
+                        regexp: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z@#$%^&*!~_-]{6,32}$/,
+                        message: "由字母、数字或特殊字符如：@#$%^&*!~ 组成,要求6-32位，必须要包含数字，大小写字母"
+                    }
+                }
+            },
+            'reset-password-repeat': {
+                validators: {
+                    notEmpty: {
+                        message:'密码不能为空'
+                    },
+                    identical: {
+                        field: 'reset-password',
+                        message: '两次输入密码不同'
+                    }
+                }
+            }
+        }
+    }).on('success.form.bv', function(e) {
+        e.preventDefault();
+        var postdata = {
+            "username":$("#reset-password-username").val(),
+            "password":$("[name = 'reset-password']").val(),
+            "dbId":$("#reset-password-username").attr('data-db-id')
+        }
+        var url = "/dbUser/security/"+$("#reset-password-username").val();
+    	$.post(url,postdata, function(data) {
+    		if(error(data)){return;}
+    		$('#reset-password-box').modal('hide');
+    	});
+    }).on('keyup', '[name="reset-password"]', function () {
+        if($("[name = 'reset-password-repeat']").val() != ''){
+            $('#reset-password-form').bootstrapValidator('revalidateField', 'reset-password-repeat');
+            $("#reset-password-box").modal({show:false});
+        }
+    })
+}
+function InitDoubleFrame(id,data){
+    var $d = $(id);		                    //双选框div
+    $d.find("li").remove();
+    $sl = $d.find(".select-list-left");		//左选框ul
+    $sr = $d.find(".select-list-right");	//右选框ul
+    for(var i= 0,len=data.length;i<len;i++){
+        if(data[i].used == 0){
+            AddToLeftFrame($sl,data[i]);
+        }else{
+            AddToRightFrame($sr,data[i]);
+        }
+    };
+    /*初始化选中操作*/
+    SelectToggle($d);
+    /*双击移动操作*/
+    DoubleClickToggle($d);
 
+    /*添加选中内容*/
+    $d.find(".btn_db_add").unbind('click').click(function(){
+        var selected= [];
+        $sl.find(".active").each(function (i,val) {
+            selected.push({addr:$(val).html()});
+        });
+        $sl.find(".active").remove();
+        for(var i= 0,len=selected.length;i<len;i++){
+            AddToRightFrame($sr,selected[i]);
+        }
+        SelectToggle($d);
+        DoubleClickToggle($d);
+    });
+    $d.find(".btn_db_remove").unbind('click').click(function(){
+        var selected= [];
+        $sr.find(".active").each(function (i,val) {
+            selected.push({addr:$(val).find("p:first").html()});
+        });
+        $sr.find(".active").remove();
+        for(var i= 0,len=selected.length;i<len;i++){
+            AddToLeftFrame($sl,selected[i]);
+        }
+        SelectToggle($d);
+        DoubleClickToggle($d);
+    });
+    /*全选按钮初始化*/
+    $d.find(".select-all-rw").unbind('click').click(function(){
+        var $sr = $(this).closest(".mcluster-select");
+        if($(this).html() == "全部设管理"){
+            $(this).html("全部设读写");
+            $sr.find(".mgr").prev().click();
+        }else if ($(this).html() == "全部设读写"){
+            $(this).html("全部设只读");
+            $sr.find(".rw").prev().click();
+        }else{
+            $(this).html("全部设管理");
+            $sr.find(".ro").prev().click();
+        }
+    });
+
+}
 /*function searchAction(){
 	$('#nav-search-input').bind('keypress',function(event){
         if(event.keyCode == "13")    
@@ -294,4 +521,6 @@ function pageControl() {
 function page_init(){
 	queryByPage();
 	pageControl();
+	formValidate();
 }
+
