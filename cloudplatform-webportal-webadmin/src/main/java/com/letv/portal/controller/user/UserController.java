f52fbd5b8381d1cf71a4b83a91b50468e75c6a0b
lisuxiao <lisuxiao@CNBJWK43997G51.letv.local>
2015-07-07 15:25:12 +0800

    添加查询所有用户接口，调用地址：/user，方式GET

---
+++ cloudplatform-webportal-webadmin/src/main/java/com/letv/portal/controller/user/UserController.java
@@ -0,0 +1,38 @@
+package com.letv.portal.controller.user;
+
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.letv.common.paging.impl.Page;
+import com.letv.common.result.ResultObject;
+import com.letv.common.util.HttpUtil;
+import com.letv.portal.service.IUserService;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	@Resource
+	private IUserService  userService;
+
+	
+	private final static Logger logger = LoggerFactory.getLogger(UserController.class);
+	
+	@RequestMapping(method=RequestMethod.GET)   
+	public @ResponseBody ResultObject listAll(Page page,HttpServletRequest request,ResultObject obj) {
+		Map<String,Object> params = HttpUtil.requestParam2Map(request);
+		obj.setData(this.userService.selectByMap(params));
+		return obj;
+	}
+	
+
+	
+}
