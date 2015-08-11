52b042abcc979f141b7ca06ace1b4ec5974f5ebb
yaokuo <yaokuo@letv.com>
2015-08-11 11:00:10 +0800

    添加最新监控时间提示

--- cloudplatform-webportal-webadmin/src/main/webapp/static/scripts/pagejs/rds/rds_monitor_node_health.js
+++ cloudplatform-webportal-webadmin/src/main/webapp/static/scripts/pagejs/rds/rds_monitor_node_health.js
@@ -119,10 +119,13 @@ function updateHealthData() {
 				td.push("<td>"+dataErrorFilter(Math.ceil(array[i].slowQueryCount))+"</td>");
 				td.push("<td>"+dataErrorFilter(array[i].cpu)+"</td>");
 				td.push("<td>"+dataErrorFilter(array[i].memory)+"</td>");
-				var tr ="<tr>"+td.join('')+"</tr>";
+				var tr ="<tr data-toggle='tooltip' data-placement='top' title='最新监控时间:"+date('Y-m-d H:i:s',array[i].updateTime)+"'>"+td.join('')+"</tr>";
 				trs.push(tr);
 			}//循环json中的数据 
 			tby.html(trs);
+			var tooltip = $('[data-toggle = "tooltip"]').tooltip({
+				position: { my: "right top+40%", at: "right" }
+			});
 			if (totalPages <= 1) {
 				$("#pageControlBar").hide();
 			} else {
--- cloudplatform-webportal-webadmin/src/main/webapp/static/scripts/pagejs/rds/rds_monitor_node_innodb.js
+++ cloudplatform-webportal-webadmin/src/main/webapp/static/scripts/pagejs/rds/rds_monitor_node_innodb.js
@@ -101,10 +101,13 @@ function updateResourceData() {
 				td.push("<td>"+dataErrorFilter(array[i].innodbRowsInsert)+"</td>");
 				td.push("<td>"+dataErrorFilter(array[i].innodbRowsUpdate)+"</td>");
 				td.push("<td>"+dataErrorFilter(array[i].innodbRowsDelete)+"</td>");
-				var tr ="<tr>"+td.join('')+"</tr>";
+				var tr ="<tr  data-toggle='tooltip' data-placement='top' title='最新监控时间:"+date('Y-m-d H:i:s',array[i].updateTime)+"'>"+td.join('')+"</tr>";
 				trs.push(tr);
 			}//循环json中的数据 
 			tby.html(trs);
+			var tooltip = $('[data-toggle = "tooltip"]').tooltip({
+				position: { my: "right top+40%", at: "right" }
+			});
 			if (totalPages <= 1) {
 				$("#pageControlBar").hide();
 			} else {
