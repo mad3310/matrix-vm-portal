c5dee5d1f48ccf877cfd1fe9bef4d3c1a0c867ee
jiangfei5945 <jiangfei5945@hotmail.com>
2015-09-16 11:03:43 +0800

    提交form表单的验证

--- cloudplatform-webportal-webapp/src/main/webapp/static/apps/cloudvm/directives/directives.js
+++ cloudplatform-webportal-webapp/src/main/webapp/static/apps/cloudvm/directives/directives.js
@@ -28,5 +28,41 @@ define(['directives/app.directive'],function (directiveModule) {
             templateUrl: '/app/directives/le-modal/template.html'
         };
     });
+    directiveModule.directive("passwordVerify", function() {
+        return {
+            require: "ngModel",
+            scope: {
+                passwordVerify: '='
+            },
+            link: function(scope, element, attrs, ctrl) {
+                var isInit=true
+                scope.$watch(function() {
+                    var combined;
 
+                    if (scope.passwordVerify || ctrl.$viewValue) {
+                        combined = scope.passwordVerify + '_' + ctrl.$viewValue;
+                    }
+                    return combined;
+                }, function(value) {
+                    if(isInit){
+                        isInit=false;
+                        ctrl.$setValidity("passwordVerify", true);
+                        return true;
+                    }
+                    if (value) {
+                        ctrl.$parsers.unshift(function(viewValue) {
+                            var origin = scope.passwordVerify;
+                            if (origin !== viewValue) {
+                                ctrl.$setValidity("passwordVerify", false);
+                                return undefined;
+                            } else {
+                                ctrl.$setValidity("passwordVerify", true);
+                                return viewValue;
+                            }
+                        });
+                    }
+                });
+            }
+        };
+    });
 });
