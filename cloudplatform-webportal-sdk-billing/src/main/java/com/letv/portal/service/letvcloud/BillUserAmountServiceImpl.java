536b7b746f660dea6cd543d9f4324ab9697937bc
liuhao1 <liuhao1@letv.com>
2015-09-17 19:20:38 +0800

    提供用户api、消息api、账户金额api

--- cloudplatform-webportal-sdk-billing/src/main/java/com/letv/portal/service/letvcloud/BillUserAmountServiceImpl.java
+++ cloudplatform-webportal-sdk-billing/src/main/java/com/letv/portal/service/letvcloud/BillUserAmountServiceImpl.java
@@ -36,10 +36,7 @@ public class BillUserAmountServiceImpl implements BillUserAmountService {
 
     @Override
     public void createUserAmount(Long userId) throws CommonException {
-        long ret = Long.valueOf(billUserAmountMapper.insertUserAmountDefault(userId));
-        if (ret < 1) {
-            throw new CommonException("创建用户余额失败");
-        }
+        billUserAmountMapper.insertUserAmountDefault(userId);
     }
 
     @Override
--- cloudplatform-webportal-webapp/src/main/java/com/letv/portal/controller/user/UserController.java
+++ cloudplatform-webportal-webapp/src/main/java/com/letv/portal/controller/user/UserController.java
@@ -7,12 +7,13 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,8 +23,6 @@ import com.letv.common.result.ResultObject;
 import com.letv.common.session.Session;
 import com.letv.common.util.CookieUtil;
 import com.letv.common.util.HttpsClient;
-import com.letv.portal.service.openstack.OpenStackService;
-import com.mysql.jdbc.StringUtils;
 
 
 /**Program Name: UserController <br>
@@ -39,62 +38,116 @@ public class UserController {
 	
 	@Value("${uc.auth.http}")
 	private String UC_AUTH_HTTP;
-	@Autowired
-	private OpenStackService openStackService;
+	@Value("${uc.auth.api.http}")
+	private String UC_AUTH_API_HTTP;
 	
 	private final static Logger logger = LoggerFactory.getLogger(UserController.class);
 	
+	/**Methods Name: userInfo <br>
+	 * Description: 根据当前session获取用户信息<br>
+	 * @author name: liuhao1
+	 * @param request
+	 * @param response
+	 * @param obj
+	 * @return
+	 * @throws Exception
+	 */
 	@RequestMapping(method=RequestMethod.GET)   
-	public @ResponseBody ResultObject account(HttpServletRequest request,HttpServletResponse response,ResultObject obj) throws Exception{
-		Session session = (Session) request.getSession().getAttribute(Session.USER_SESSION_REQUEST_ATTRIBUTE);
+	public @ResponseBody ResultObject userInfo(HttpServletRequest request,HttpServletResponse response,ResultObject obj) throws Exception{
 		Cookie ucCookie = CookieUtil.getCookieByName(request, Session.UC_COOKIE_KEY);
-		
-		if(session == null && ucCookie !=null) {
-			session = getUserdetailinfo(ucCookie.getValue(),request);
-		}
-		
-		if(session == null ) {
+		if(ucCookie == null) {
 			obj.setResult(0);
-		} else {
-			obj.setData(session.getUserVo());
+			return obj;
 		}
-		
+		Map<String, Object> userdetailinfo = this.getUserByCookieId(ucCookie.getValue());
+		if(userdetailinfo == null || userdetailinfo.isEmpty()) {
+			obj.setResult(0);
+			return obj;
+		}
+		obj.setData(userdetailinfo);
 		return obj;
 	}
 	
-	private Session getUserdetailinfo(String ucCookieId,HttpServletRequest request) throws Exception{
-		Map<String, Object> userdetailinfo = this.getUserdetailinfo(ucCookieId);
-		if(null == userdetailinfo || userdetailinfo.isEmpty()||null == userdetailinfo.get("id"))
-			return null;
-		
-		Long userId = Long.valueOf((Integer) userdetailinfo.get("id"));
-		String username = (String) userdetailinfo.get("contacts");
-		String email = (String) userdetailinfo.get("email");
-		String mobile = (String) userdetailinfo.get("mobile");
-		Integer mobileStatus = (Integer) userdetailinfo.get("mobileStatus");
-		
-		Session session = new Session(userId);
-		session.setUserName(username);
-		session.setEmail(email);
-		session.setMobile(mobile);
-		
-		session.setUserVo(new UserVo(userId,username,email,mobile,mobileStatus));
-		
-		session.setOpenStackSession(openStackService.createSession(email,email,username));
-		return session;
+	/**Methods Name: userInfo <br>
+	 * Description: 根据userId，获取用户信息<br>
+	 * @author name: liuhao1
+	 * @param userId
+	 * @param obj
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value="/{userId}",method=RequestMethod.GET)   
+	public @ResponseBody ResultObject userInfo(@PathVariable Long userId,ResultObject obj) throws Exception{
+		if(null ==userId) {
+			obj.setResult(0);
+			return obj;
+		}
+		Map<String, Object> userdetailinfo = this.getUserByUserId(userId);
+		if(userdetailinfo == null || userdetailinfo.isEmpty()) {
+			obj.setResult(0);
+			return obj;
+		}
+		obj.setData(userdetailinfo);
+		return obj;
+	}
+	@RequestMapping(value="/message/un/{userId}",method=RequestMethod.GET)   
+	public @ResponseBody ResultObject unReadMessage(@PathVariable Long userId,ResultObject obj) throws Exception{
+		if(null ==userId) {
+			obj.setResult(0);
+			return obj;
+		}
+		Map<String, Object> unReadMessage = this.getUnReadMessage(userId);
+		if(unReadMessage == null || unReadMessage.isEmpty()) {
+			obj.setResult(0);
+			return obj;
+		}
+		obj.setData(unReadMessage);
+		return obj;
+	}
+	@RequestMapping(value="/order/{userId}",method=RequestMethod.GET)   
+	public @ResponseBody ResultObject order(@PathVariable Long userId,ResultObject obj) throws Exception{
+		if(null ==userId) {
+			obj.setResult(0);
+			return obj;
+		}
+		Map<String, Object> userdetailinfo = this.getUserByUserId(userId);
+		if(userdetailinfo == null || userdetailinfo.isEmpty()) {
+			obj.setResult(0);
+			return obj;
+		}
+		obj.setData(userdetailinfo);
+		return obj;
 	}
 	
-	private Map<String,Object> getUserdetailinfo(String ucCookieId) {
+	private Map<String,Object> getUnReadMessage(Long userId) {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append(UC_AUTH_API_HTTP).append("/unReadMessage.do?userId=").append(userId);
+		logger.info("getUnReadMessage url:{}",buffer.toString());
+		String result = HttpsClient.sendXMLDataByGet(buffer.toString(),1000,2000);
+		Map<String,Object> resultMap = this.transResult(result);
+		return resultMap;
+	}
+	
+	private Map<String,Object> getUserByUserId(Long userId) {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append(UC_AUTH_API_HTTP).append("/userInfoById.do?userId=").append(userId);
+		logger.info("getUserDetailInfo url:{}",buffer.toString());
+		String result = HttpsClient.sendXMLDataByGet(buffer.toString(),1000,2000);
+		Map<String,Object> resultMap = this.transResult(result);
+		return resultMap;
+	}
+	
+	private Map<String,Object> getUserByCookieId(String ucCookieId) {
 		StringBuffer buffer = new StringBuffer();
 		buffer.append(UC_AUTH_HTTP).append("/user/userInfo.do?sessionId=").append(ucCookieId);
 		String result = HttpsClient.sendXMLDataByGet(buffer.toString(),1000,2000);
-		if(StringUtils.isNullOrEmpty(result))
-			throw new OauthException("getUserdetailinfo connection timeout");
 		Map<String,Object> resultMap = this.transResult(result);
 		return resultMap;
 	}
 	
 	private Map<String,Object> transResult(String result){
+		if(StringUtils.isEmpty(result))
+			return null;
 		ObjectMapper resultMapper = new ObjectMapper();
 		Map<String,Object> jsonResult = new HashMap<String,Object>();
 		try {
