7bbef8a7e9d7e0eb8d64da5ea85aa1298568c90a
lisuxiao <lisuxiao@CNBJWK43997G51.letv.local>
2015-09-19 17:37:11 +0800

    添加getStandardsInfoByProductElements方法

--- cloudplatform-webportal-sdk-billing/src/main/java/com/letv/portal/service/product/impl/HostProductServiceImpl.java
+++ cloudplatform-webportal-sdk-billing/src/main/java/com/letv/portal/service/product/impl/HostProductServiceImpl.java
@@ -1,20 +1,32 @@
 package com.letv.portal.service.product.impl;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.letv.portal.dao.base.IBaseStandardDao;
+import com.letv.portal.dao.product.IProductElementDao;
+import com.letv.portal.model.base.BaseStandard;
+import com.letv.portal.model.product.ProductElement;
 import com.letv.portal.service.product.IHostProductService;
 
 @Service("hostProductService")
 public class HostProductServiceImpl extends ProductServiceImpl implements IHostProductService {
 	
 	private final static Logger logger = LoggerFactory.getLogger(HostProductServiceImpl.class);
+	
+	@Autowired
+	private IProductElementDao productElementDao;
+	@Autowired
+	private IBaseStandardDao baseStandardDao;
 	
 	/**
 	  * @Title: validateData
@@ -37,7 +49,7 @@ public class HostProductServiceImpl extends ProductServiceImpl implements IHostP
 		Map<String, Set<String>> standards = new HashMap<String, Set<String>>();
 		Map<String, String> chargeTypes = new HashMap<String, String>();
 		
-		super.getStandardsInfoByProductElements(id, map, standards, chargeTypes);
+		getStandardsInfoByProductElements(id, map, standards, chargeTypes);
 		
 		for (String standard : standards.keySet()) {
 			if(chargeTypes.get(standard)==null) {
@@ -74,6 +86,41 @@ public class HostProductServiceImpl extends ProductServiceImpl implements IHostP
 			return false;
 		}
 		return true;
+	}
+	
+	@Override
+	protected void getStandardsInfoByProductElements(Long id,
+			Map<String, Object> map, Map<String, Set<String>> standards,
+			Map<String, String> chargeTypes) {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.clear();
+		params.put("productId", id);
+		List<ProductElement> productElements = this.productElementDao.selectByMap(params);
+		for (ProductElement productElement : productElements) {
+			params.clear();
+			params.put("baseElementId", productElement.getBaseElementId());
+			List<BaseStandard> baseStandards = this.baseStandardDao.selectByMap(params);
+			for (BaseStandard baseStandard : baseStandards) {
+				Set<String> set = null;
+				if(standards.containsKey(baseStandard.getStandard())) {
+					set = standards.get(baseStandard.getStandard());
+					if(baseStandard.getBasePrice()!=null && (Integer.parseInt(baseStandard.getBasePrice().getType())==1 || Integer.parseInt(baseStandard.getBasePrice().getType())==2
+							 || Integer.parseInt(baseStandard.getBasePrice().getType())==3)) {//阶梯定价或线性定价
+						if(Double.parseDouble(set.iterator().next())<Double.parseDouble(baseStandard.getValue())) {//只放入最大值
+							set.clear();
+						}
+					}
+					set.add(baseStandard.getValue());
+				} else {
+					set = new HashSet<String>();
+					set.add(baseStandard.getValue());
+					standards.put(baseStandard.getStandard(), set);
+				}
+			}
+			if(baseStandards!=null && baseStandards.size()!=0 && baseStandards.get(0).getBasePrice()!=null) {
+				chargeTypes.put(baseStandards.get(0).getStandard(), baseStandards.get(0).getBasePrice().getType());
+			}
+		}
 	}
 
 }
