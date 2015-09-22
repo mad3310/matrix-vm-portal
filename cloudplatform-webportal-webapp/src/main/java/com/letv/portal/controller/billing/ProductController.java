75717b7b975622642a5975ed383d1384a62db218
lisuxiao <lisuxiao@CNBJWK43997G51.letv.local>
2015-09-22 15:39:27 +0800

    返回结果由订单id变为订单编号

--- cloudplatform-webportal-webapp/src/main/java/com/letv/portal/controller/billing/ProductController.java
+++ cloudplatform-webportal-webapp/src/main/java/com/letv/portal/controller/billing/ProductController.java
@@ -137,7 +137,7 @@ public class ProductController {
 						o.setStatus(0);
 						o.setCreateUser(sessionService.getSession().getUserId());
 						this.orderService.insert(o);
-						obj.setData(o.getId());
+						obj.setData("orderNumber:"+o.getOrderNumber());
 					}
 					
 					List<SubscriptionDetail> subDetails = this.subscriptionDetailService.selectByMapAndTime(sub.getId());
