5b77e8faf4231944fb5b11b1b68807c4af40a595
zhouxianguang <zhouxianguang@letv.com>
2015-09-23 13:44:03 +0800

    fix bug of get session

--- cloudplatform-webportal-openstack/src/main/java/com/letv/portal/service/openstack/billing/VmCreateListener.java
+++ cloudplatform-webportal-openstack/src/main/java/com/letv/portal/service/openstack/billing/VmCreateListener.java
@@ -4,5 +4,5 @@ package com.letv.portal.service.openstack.billing;
  * Created by zhouxianguang on 2015/9/22.
  */
 public interface VmCreateListener {
-    void vmCreated(String region, String vmId, int vmIndex, Object userData);
+    void vmCreated(String region, String vmId, int vmIndex, Object userData) throws Exception;
 }
