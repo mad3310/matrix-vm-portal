96dd86cf78573b4a4e78e16480e39d9f10c525f8
gaomin <gaomin@CNBJWK43997GY.letv.local>
2015-09-24 14:19:59 +0800

    概览页 收起&&展示 功能 bugfix

--- cloudplatform-webportal-webapp/src/main/webapp/static/apps/dashboard/controllers/dashboard.js
+++ cloudplatform-webportal-webapp/src/main/webapp/static/apps/dashboard/controllers/dashboard.js
@@ -13,7 +13,13 @@ define(['controllers/app.controller'], function (controllerModule) {
 			$scope.ecs_sleep='';
 			$scope.expander.layout= "normal";
 
-			$scope.expanderToggle = function(){
+			$scope.expanderToggle = function(element){
+				var _target=element.target||element.srcElement;
+				if($(_target).hasClass('zhankai')){
+					$(_target).removeClass('zhankai').text('更多')
+				}else{
+					$(_target).addClass('zhankai').text('收起')
+				}
 				$scope.expander.layout === "top-expander"? $scope.expander.layout = "top-shrink":$scope.expander.layout="top-expander";
 			}
 			var inite=function(){
