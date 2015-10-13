05ec9ad1ffa3da3c03e6018a14c68c841f2ad602
zhouxianguang <zhouxianguang@letv.com>
2015-10-13 14:41:53 +0800

    check volume status before attaching volume to vm

--- cloudplatform-webportal-openstack/src/main/java/com/letv/portal/service/openstack/resource/manager/impl/create/vm/AddVolumeTask.java
+++ cloudplatform-webportal-openstack/src/main/java/com/letv/portal/service/openstack/resource/manager/impl/create/vm/AddVolumeTask.java
@@ -1,12 +1,20 @@
 package com.letv.portal.service.openstack.resource.manager.impl.create.vm;
 
 import com.letv.portal.service.openstack.exception.OpenStackException;
+import com.letv.portal.service.openstack.resource.manager.impl.Checker;
+import org.jclouds.openstack.cinder.v1.domain.Volume;
 
 public class AddVolumeTask implements VmsCreateSubTask {
 
 	@Override
 	public void run(MultiVmCreateContext context) throws OpenStackException {
 		for (VmCreateContext vmCreateContext : context.getVmCreateContexts()) {
+			context.getVolumeManager().waitingVolume(context.getApiCache().getVolumeApi(), vmCreateContext.getVolume().getId(), 1000, new Checker<Volume>() {
+				@Override
+				public boolean check(Volume volume) throws Exception {
+					return volume == null || volume.getStatus() == Volume.Status.AVAILABLE;
+				}
+			});
 			context.getApiCache()
 					.getVolumeAttachmentApi()
 					.attachVolumeToServerAsDevice(
