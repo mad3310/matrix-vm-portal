5e8158927fcdb8010ff17a6d84e3a17ef09044fa
zhouxianguang <zhouxianguang@letv.com>
2015-10-16 15:52:19 +0800

    provide fake controller methods for image and vm snapshot

--- cloudplatform-webportal-webapp/src/main/java/com/letv/portal/controller/cloudvm/VMController.java
+++ cloudplatform-webportal-webapp/src/main/java/com/letv/portal/controller/cloudvm/VMController.java
@@ -2,11 +2,14 @@ package com.letv.portal.controller.cloudvm;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import com.letv.common.paging.impl.Page;
 import com.letv.portal.constant.Constant;
 import com.letv.portal.service.openstack.exception.UserOperationException;
 import com.letv.portal.service.openstack.local.query.service.VmQueryService;
 
+import com.letv.portal.service.openstack.util.Params;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -457,4 +460,42 @@ public class VMController {
 		}
 		return result;
 	}
+
+	@RequestMapping(value = "/vm/snapshot/list", method = RequestMethod.GET)
+	public
+	@ResponseBody
+	ResultObject listVmSnapshot(@RequestParam String region, @RequestParam(required = false) String name,
+								@RequestParam(required = false) Integer currentPage,
+								@RequestParam(required = false) Integer recordsPerPage) {
+		Params obj = new Params().p("id", "fake-id").p("name","fake vm snapshot").p("region","cn-beijing-1").p("createdAt",11111111).p("size",1).p("status","ACTIVE").p("minRam",1).p("minDisk",1);
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+		list.add(obj);
+		Page page;
+		if (currentPage == null || recordsPerPage == null) {
+			page = new Page();
+		} else {
+			page = new Page(currentPage, recordsPerPage);
+		}
+		page.setData(list);
+		ResultObject result = new ResultObject();
+		result.setData(page);
+		return result;
+	}
+
+	@RequestMapping(value = "/vm/snapshot/create", method = RequestMethod.GET)
+	public
+	@ResponseBody
+	ResultObject createVmSnapshot(@RequestParam String region, @RequestParam String vmId, @RequestParam String name) {
+		ResultObject result = new ResultObject();
+		return result;
+	}
+
+	@RequestMapping(value = "/vm/snapshot/delete", method = RequestMethod.GET)
+	public
+	@ResponseBody
+	ResultObject deleteVmSnapshot(@RequestParam String region, @RequestParam String vmSnapshotId) {
+		ResultObject result = new ResultObject();
+		return result;
+	}
+
 }
