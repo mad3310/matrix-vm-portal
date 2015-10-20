04de2ba3860d8528c196cfafe87a527807e53d62
zhouxianguang <zhouxianguang@letv.com>
2015-10-20 15:44:33 +0800

    support FloatingIpCreateFailEvent,RouterCreateFailEvent,VmSnapshotCreateFailEvent

--- cloudplatform-webportal-openstack/src/main/java/com/letv/portal/service/openstack/billing/listeners/RouterCreateListener.java
+++ cloudplatform-webportal-openstack/src/main/java/com/letv/portal/service/openstack/billing/listeners/RouterCreateListener.java
@@ -1,6 +1,7 @@
 package com.letv.portal.service.openstack.billing.listeners;
 
 import com.letv.portal.service.openstack.billing.listeners.event.RouterCreateEvent;
+import com.letv.portal.service.openstack.billing.listeners.event.RouterCreateFailEvent;
 
 /**
  * Created by zhouxianguang on 2015/10/8.
@@ -9,4 +10,6 @@ public interface RouterCreateListener {
     void routerCreated(String region, String routerId, int routerIndex, Object userData) throws Exception;
 
     void routerCreated(RouterCreateEvent event) throws Exception;
+
+    void routerCreateFailed(RouterCreateFailEvent event) throws Exception;
 }
