0da1b0f526bc51a6342c5c20857991fc89c0df19
zhouxianguang <zhouxianguang@letv.com>
2015-11-05 15:47:29 +0800

    change volme type order

--- cloudplatform-webportal-data-model/src/main/java/com/letv/portal/model/cloudvm/CloudvmVolumeType.java
+++ cloudplatform-webportal-data-model/src/main/java/com/letv/portal/model/cloudvm/CloudvmVolumeType.java
@@ -6,8 +6,8 @@ import com.letv.portal.model.adminoplog.IntEnum;
  * Created by zhouxianguang on 2015/10/21.
  */
 public enum CloudvmVolumeType implements IntEnum {
-    SAS(1, "709c9914-7056-4211-a03c-a41b056f3e8c", "性能型", 1L, 1L),
     SATA(2, "d3e50993-e9a7-415b-bbb7-f0b2e4e7dc50", "容量型", 1L, 1L),
+    SAS(1, "709c9914-7056-4211-a03c-a41b056f3e8c", "性能型", 1L, 1L),
     SSD(3, "9f12c2c2-4db4-40ae-b1ef-0f44c2f3c7e8", "高性能型", 1L, 1L);
 
     private Integer code;
