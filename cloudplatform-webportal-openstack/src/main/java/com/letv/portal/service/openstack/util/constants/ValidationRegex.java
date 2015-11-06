f01aa05dc8cee6a5c45ffbfb1cfcee46b6f21282
zhouxianguang <zhouxianguang@letv.com>
2015-11-06 12:06:42 +0800

    change regex of validations

--- cloudplatform-webportal-openstack/src/main/java/com/letv/portal/service/openstack/util/constants/ValidationRegex.java
+++ cloudplatform-webportal-openstack/src/main/java/com/letv/portal/service/openstack/util/constants/ValidationRegex.java
@@ -7,7 +7,7 @@ public class ValidationRegex {
     public static final String password = "(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[a-zA-Z0-9]{8,30}";
     public static final String passwordMessage = "8-30个字符，同时包含大小写字母和数字，不支持特殊符号";
 
-    public static final String name = "^[\u0000-\uFFFFa-zA-Z][^@/:=\"<>\\{\\}\\[\\]\\s]{1,127}$";
+    public static final String name = "^[([a-zA-Z]|[^[^u4e00-u9fa5]])][^@/:=\"<>\\{\\}\\[\\]\\s]{1,127}$";
     public static final String nameMessage = "名称须为2-128个字符，以大小写字母或中文开头，不支持字符@/:=\\\"<>{[]}和空格";
 
     public static final String keyPairName = "^[a-zA-Z][^[@/:=\\\\\"<>\\{\\[\\]\\}\\su4e00-u9fa5]]{1,127}$";
