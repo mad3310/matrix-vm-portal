6ed665cbe943beec10baf28049b8af5071f018cf
yaokuo <yaokuo@letv.com>
2015-11-20 15:07:33 +0800

    修复名称过长显示异常问题

--- cloudplatform-webportal-webapp/src/main/webapp/static/apps/common/services/HttpService.js
+++ cloudplatform-webportal-webapp/src/main/webapp/static/apps/common/services/HttpService.js
@@ -10,7 +10,7 @@ define(['./common.service'], function (serviceModule) {
           if(data.result == 0){
             WidgetService.notifyError(data.msgs[0]||'获取数据失败');
           }else if(data.result == 2){
-            $window.location.reload();
+            window.location.reload();
           }
           return;
         })
