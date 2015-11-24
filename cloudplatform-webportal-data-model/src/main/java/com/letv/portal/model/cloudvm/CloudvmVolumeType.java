86d8ef73792ecc335e6fafae98ada7cbabe6f53d
zhouxianguang <zhouxianguang@letv.com>
2015-11-24 16:01:24 +0800

    change capacity of CloudvmVolumeType

--- cloudplatform-webportal-data-model/src/main/java/com/letv/portal/model/cloudvm/CloudvmVolumeType.java
+++ cloudplatform-webportal-data-model/src/main/java/com/letv/portal/model/cloudvm/CloudvmVolumeType.java
@@ -6,9 +6,9 @@ import com.letv.portal.model.adminoplog.IntEnum;
  * Created by zhouxianguang on 2015/10/21.
  */
 public enum CloudvmVolumeType implements IntEnum {
-    SATA(2, "d3e50993-e9a7-415b-bbb7-f0b2e4e7dc50", "容量型", 1L, 1L, true, 2048L),
-    SAS(1, "709c9914-7056-4211-a03c-a41b056f3e8c", "性能型", 1L, 1L, false, 2048L),
-    SSD(3, "9f12c2c2-4db4-40ae-b1ef-0f44c2f3c7e8", "高性能型", 1L, 1L, false, 2048L);
+    SATA(2, "d3e50993-e9a7-415b-bbb7-f0b2e4e7dc50", "容量型", 1L, 1L, true, 2000L),
+    SAS(1, "709c9914-7056-4211-a03c-a41b056f3e8c", "性能型", 1L, 1L, false, 2000L),
+    SSD(3, "9f12c2c2-4db4-40ae-b1ef-0f44c2f3c7e8", "高性能型", 1L, 1L, false, 2000L);
 
     private Integer code;
     private String volumeTypeId;
