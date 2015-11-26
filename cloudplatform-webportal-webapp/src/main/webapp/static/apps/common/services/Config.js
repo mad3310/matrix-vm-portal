6f2d649c6a65e6263bda01c7b872cdd298db8a2c
jiangfei5945 <jiangfei5945@hotmail.com>
2015-11-26 17:29:57 +0800

    修改名称的校验逻辑

--- cloudplatform-webportal-webapp/src/main/webapp/static/apps/common/services/Config.js
+++ cloudplatform-webportal-webapp/src/main/webapp/static/apps/common/services/Config.js
@@ -78,13 +78,13 @@ define(['./common.service'], function (serviceModule) {
       keypair_check:'/ecs/keypair/create/check',
     };
     config.REGEX= {
-      NAME: /^[a-zA-Z\u4e00-\u9fa5][^\s"@\/:=<>{\[\]}]{1,127}$/,
-      NAME_NO_ZH: /^[a-zA-Z][^\s"@\/:=<>{\[\]}\u4e00-\u9fa5]{1,127}$/,
+      NAME: /^[a-zA-Z\u4e00-\u9fa5][a-zA-Z\u4e00-\u9fa5\(（_\-\)）]{1,127}$/,
+      NAME_NO_ZH: /^[a-zA-Z][a-zA-Z(（_\-\)）]{1,127}$/,
       PASSWORD: /^(?=.*[0-9].*)(?=.*[A-Z].*)(?=.*[a-z].*)[a-zA-Z0-9]{8,30}$/,
     };
     config.REGEX_MESSAGE= {
-      NAME: '名称须为2-128个字符，以大小写字母或中文开头，不支持字符@/:="<>{[]}和空格',
-      NAME_NO_ZH: '名称须为2-128个字符，以大小写字母开头，不支持字符@/:="<>{[]}和空格以及中文',
+      NAME: '名称须为2-128个字符，支持大小写字母或中文以及()（）_-，以大小写字母或中文开头',
+      NAME_NO_ZH: '名称须为2-128个字符，支持大小写字母以及()（）_-，以大小写字母开头',
       PASSWORD: '8-30个字符，同时包含大小写字母和数字，不支持特殊符号',
     };
     config.vmStatuses = {
