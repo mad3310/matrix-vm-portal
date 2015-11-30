04c033111c9f3aef79add195e53cfe706ab5fdf0
lisuxiao <lisuxiao@CNBJWK43997G51.letv.local>
2015-11-30 14:19:25 +0800

    修改邀请码中验证码出现逻辑

--- cloudplatform-webportal-webapp/src/main/java/com/letv/portal/interceptor/InviteCodeInterceptor.java
+++ cloudplatform-webportal-webapp/src/main/java/com/letv/portal/interceptor/InviteCodeInterceptor.java
@@ -3,7 +3,6 @@ package com.letv.portal.interceptor;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
