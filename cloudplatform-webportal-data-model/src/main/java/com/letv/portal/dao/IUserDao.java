ebf2d0ef877c86c7812ee9a59401d2c0392b381c
liuhao1 <liuhao1@letv.com>
2015-12-06 16:45:02 +0800

    update cbase and rds url to 127.0.0.1

--- cloudplatform-webportal-data-model/src/main/java/com/letv/portal/dao/IUserDao.java
+++ cloudplatform-webportal-data-model/src/main/java/com/letv/portal/dao/IUserDao.java
@@ -2,7 +2,9 @@ package com.letv.portal.dao;
 
 import com.letv.common.dao.IBaseDao;
 import com.letv.portal.model.UserModel;
+import org.springframework.stereotype.Repository;
 
+@Repository("userDao")
 public interface IUserDao extends IBaseDao<UserModel> {
 
 	UserModel selectByOauthId(String oauthId);
