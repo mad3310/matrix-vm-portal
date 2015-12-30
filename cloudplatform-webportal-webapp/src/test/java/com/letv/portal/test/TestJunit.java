32f5c03d11728b41d8be429c39003a5687cb1591
liuhao1 <liuhao1@letv.com>
2015-12-30 11:02:26 +0800

    fix junit test bug.

---
+++ cloudplatform-webportal-webapp/src/test/java/com/letv/portal/test/TestJunit.java
@@ -0,0 +1,15 @@
+package com.letv.portal.test;
+
+import com.letv.portal.junitBase.AbstractTest;
+import org.junit.Test;
+
+/**
+ * Created by liuhao1 on 2015/12/30.
+ */
+public class TestJunit extends AbstractTest {
+
+    @Test
+    public void hello() {
+        System.out.println("hello,world");
+    }
+}
